587854b88d7231bd89aa78aa68f8a86d8957ba28
semeraro <dave.semeraro@gmail.com>
2023-05-24 16:33:46 -0500

    added file and domain io to the cloud application

--- src/CloudRender/Cloud.cpp
+++ src/CloudRender/Cloud.cpp
@@ -6,14 +6,24 @@
 // Using classes from the OpenVKL/Examples where appropriate.
 //
 #include "renderer/Scene.h"
+#include "IO/Domain.h"
+#include <openvkl/openvkl.h>
 using namespace openvkl::examples;
-
 int main(int argc, char **argv) {
     
     std::cout << " Starting ... " << std::endl;
     initializeOpenVKL();
     Scene scene;
+    // read the data
     std::list<std::string> args(argv,argv+argc);
+    std::string directory("C:\\Users\\Dave Semeraro\\Documents\\VolumeRendering\\Data\\");
+    std::string filename("cloud.bov");
+    std::string filespec = directory+filename;
+    std::string fieldname("qi");
+    std::cout << "Opening " + filespec + " " << std::endl;
+    Domain Storm(filespec,fieldname);
+    VKLVolume vol = DomainToVolume(Storm,getOpenVKLDevice());
+    //
     if(!scene.parseCommandLine(args)) {
         std::cout << "parseCommandLine yacked " << std::endl;
         return 0;
