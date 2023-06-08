6a10ffc8fa21cf10a9bd47cfc20681018519816b
semeraro <dave.semeraro@gmail.com>
2023-06-08 15:38:22 -0500

    new strategy for moving domain data to vkl volume ability to reuse the vklvolume if it exists. Passing a nullptr to the conversion function results in creation of new vklvolume object. Passing a non null volume resets the volume with new data.

--- src/IO/ToVKL.cpp
+++ src/IO/ToVKL.cpp
@@ -13,7 +13,7 @@
 
 using namespace std;
 
-VKLVolume DomainToVolume(const Domain &cloud, VKLDevice device) {
+VKLVolume DomainToVolume(const Domain &cloud, VKLDevice device, VKLVolume oldvolume) {
     int nx = cloud.coords.x;
     int ny = cloud.coords.y;
     int nz = cloud.coords.z;
@@ -24,23 +24,32 @@ VKLVolume DomainToVolume(const Domain &cloud, VKLDevice device) {
     float orgy = cloud.origin.y;
     float orgz = cloud.origin.z;
     unsigned long count = cloud.npts;
+    float bkgrnd = VKL_BACKGROUND_UNDEFINED;
     VKLDevice dev = device;
+    VKLVolume volume;
     if(!dev) {
         std::cout << "bogus device" << std::endl;
     }
-    VKLVolume volume = vklNewVolume(dev,"structuredRegular");
+    if(oldvolume==nullptr){
+        std::cout << " creating new volume in tovkl " << std::endl;
+        volume = vklNewVolume(dev,"structuredRegular");
+    } else {
+        std::cout << "using old volume " << std::endl;
+        volume = oldvolume;
+    }
     vklSetVec3i(volume,"dimensions",nx,ny,nz);
     vklSetVec3f(volume,"gridOrigin",orgx,orgy,orgz);
     vklSetVec3f(volume,"gridSpacing",spx,spy,spz);
     vklSetInt(volume,"filter",VKL_FILTER_TRILINEAR);
     vklSetInt(volume,"graientFilter",VKL_FILTER_TRILINEAR);
+    vklSetFloat(volume,"background",bkgrnd);
     VKLData data0 = vklNewData(dev,count,VKL_FLOAT,cloud.variable,VKL_DATA_SHARED_BUFFER,0);
-    VKLData attributes[] = {data0};
-    VKLData attributesData = vklNewData(dev,1,VKL_DATA,attributes,VKL_DATA_DEFAULT,0);
+    //VKLData attributes[] = {data0};
+    //VKLData attributesData = vklNewData(dev,1,VKL_DATA,attributes,VKL_DATA_DEFAULT,0);
     vklSetData(volume,"data",data0);
-    vklSetData(volume,"adata",attributesData);
+    //vklSetData(volume,"adata",attributesData);
     vklRelease(data0);
-    vklRelease(attributesData);
+    //vklRelease(attributesData);
     vklCommit(volume);
     vkl_range1f valuerange = vklGetValueRange(volume,0);
     std::cout << "Min: " << valuerange.lower << " Max: " << valuerange.upper <<std::endl;
