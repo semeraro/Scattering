eb7a2a457bd6467b342346e38f1f0f6e0dbb710c
semeraro <dave.semeraro@gmail.com>
2023-06-08 15:50:44 -0500

    Add RaycastCloud application .

---
+++ src/CloudRender/RaycastCloud.cpp
@@ -0,0 +1,9 @@
+// simple raycast volume rendering
+//
+#include "rkcommon/utility/SaveImage.h"
+#include <openvkl/openvkl.h>
+int main(int argc, char **argv) {
+    
+    std::cout << " Starting ... " << std::endl;
+    initializeOpenVKL();
+}
