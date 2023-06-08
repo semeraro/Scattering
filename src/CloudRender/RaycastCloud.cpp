ba221acc8dd94fb74426a3d93fa4306c45f93587
semeraro <dave.semeraro@gmail.com>
2023-06-08 16:25:52 -0500

    Added RaycastCloud application and mods to cmakelists to support it. RaycastCloud is a standalone cloud raycast marching renderer.

--- src/CloudRender/RaycastCloud.cpp
+++ src/CloudRender/RaycastCloud.cpp
@@ -1,9 +1,23 @@
 // simple raycast volume rendering
 //
+#include "apps/AppInit.h"
 #include "rkcommon/utility/SaveImage.h"
 #include <openvkl/openvkl.h>
+#include "IO/Domain.h"
 int main(int argc, char **argv) {
     
     std::cout << " Starting ... " << std::endl;
     initializeOpenVKL();
+    // read the data
+    // just reading the metadata
+    // letting the openvkl volume read the raw data and build a volume.
+    std::string directory("C:\\Users\\Dave Semeraro\\Documents\\VolumeRendering\\Data\\");
+    std::string filename("cloud.bov");
+    std::string filespec = directory+filename;
+    // build a domain object
+    Domain cloud = Domain(filespec,"qi",false);
+    //generate a vklVolume
+    VKLVolume vklCloud = DomainToVolume(cloud,getOpenVKLDevice());
+    
+    shutdownOpenVKL();
 }
