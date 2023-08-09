ae60d0cd1773879b1ab65d88643542a2633de7c9
semeraro <dave.semeraro@gmail.com>
2023-08-09 13:41:15 -0500

    added renderpixel, integrate and constructor to the pathtracerenderer class. Commented some functions.

--- src/renderer/PathTraceRenderer.cpp
+++ src/renderer/PathTraceRenderer.cpp
@@ -3,24 +3,64 @@
 // only using ambient light
 //
 #include "PathTraceRenderer.h"
-PathTraceRenderer::PathTraceRenderer(Domain dom) : RayMarchVolRenderer(dom) { }
+PathTraceRenderer::PathTraceRenderer(Domain dom) : RayMarchVolRenderer(dom) { 
+  // initialize the things
+  sigmaTScale = 1.f;
+  sigmaSScale = 1.f;
+  ambientLightIntensity = 1.0f;
+  maxNumScatters = 8;
+}
 void PathTraceRenderer::RenderPixel(size_t seed,Ray &r, vec4f &rgba, float &weight) {
 
-    RayMarchVolRenderer::RenderPixel(r,rgba,weight);
+    //RayMarchVolRenderer::RenderPixel(r,rgba,weight);
+    size_t frame = 1;
+    RNG rng(frame,seed);
+    int maxScatterIndex = 0;
+    vec3f color = integrate(rng,r,0,maxScatterIndex);
+    float alpha = maxScatterIndex > 0 ? 1.f : 0.f;
+    rgba.x += color.x;
+    rgba.y += color.y;
+    rgba.z += color.z;
+    rgba.w += alpha;
+    weight += 1.f;
 }
-vec3f PathTraceRenderer::integrate(RNG &rng, Ray &r , int scatterIndex, int &maxScatterIndex) {
-    return vec3f(1.0);
+vec3f PathTraceRenderer::integrate(RNG &rng, Ray &r , int scatterIndex, int &maxScatterIndex)  {
+  vec3f Le = vec3f(0.f);
+  maxScatterIndex = std::max<int>(scatterIndex,maxScatterIndex);
+  r.t = intersectRayBox(r.org,r.dir,dom_bounds);
+  if(!r.t.empty()) { // hit the bounding box.
+    float t = 0.f;
+    float sample = 0.f;
+    float transmittance = 0.f;
+    const bool haveEvent = sampleWoodcock(rng,r,r.t,t,sample,transmittance);
+    if(!haveEvent) {
+      if(scatterIndex > 0) {
+        Le += transmittance*vec3f(ambientLightIntensity);
+      }
+    } else if(scatterIndex<maxNumScatters) {
+      const vec3f p = r.org + t*r.dir;
+      Ray scatteringRay;
+      scatteringRay.t = range1f(0.f,inf);
+      scatteringRay.org = p;
+      scatteringRay.dir = uniformSampleSphere(1.f,rng.getFloats());
+      const vec3f inscatteredLe = integrate(rng,scatteringRay,scatterIndex+1,maxScatterIndex);
+      const vec4f sampleColorAndOpacity = sampleTF(sample);
+      const vec3f sigmaSSample = sigmaSScale*vec3f(sampleColorAndOpacity)*sampleColorAndOpacity.w;
+      Le = Le + sigmaSSample*inscatteredLe;
+    }
+  }
+  return Le;
 }
 bool PathTraceRenderer::sampleWoodcock(RNG &rng, Ray &r, range1f &hits, float &t, float &sample, float &transmittance) {
     t = hits.lower;
-    const float sigmaMax = sigmaTScale;
+    const float sigmaMax = sigmaTScale; // max extinction coef
     if (sigmaMax <= 0.f) {
         transmittance = 1.0;
         return false;
     }
     while(true) {
         vec2f randomNumbers = rng.getFloats();
-        vec2f randomNumbers2 = rng.getFloats();
+        //vec2f randomNumbers2 = rng.getFloats();
         t = t+-std::log(1.f-randomNumbers.x)/sigmaMax;
         if(t>hits.upper) {
             transmittance = 1.f;
@@ -50,7 +90,7 @@ void PathTraceRenderer::RenderFrame() {
           int row = idx/resolution.x; // number of pixels from top of image (y)
           int column = idx % resolution.x; // number of pixels from left edge of image (x)
           r = cam.getRay(vec2i(column,row)); // expects x,y order
-          r.t = intersectRayBox(r.org,r.dir,dom_bounds);
+          //r.t = intersectRayBox(r.org,r.dir,dom_bounds);
           vec4f &color = framebuffer[idx];
           float &wt = weightbuffer[idx];
           const size_t seed = resolution.x * static_cast<size_t>(row) + column;
@@ -58,4 +98,7 @@ void PathTraceRenderer::RenderFrame() {
         }
       }
     );
+    auto endrender = Clock::now();
+    rendertime = endrender - startrender;
+    std::cout << " render time " << rendertime.count()/1000.0 << " seconds " << std::endl;
 }
--- src/renderer/PathTraceRenderer.h
+++ src/renderer/PathTraceRenderer.h
@@ -30,13 +30,43 @@ inline float getRandomUniform()
     };
 
     using RNG = RandomSTD;
+// couple of useful functions
+// -------------------------------------------------------------------------
+
+    static vec3f cartesian(const float phi,
+                           const float sinTheta,
+                           const float cosTheta)
+    {
+      float sinPhi = std::sin(phi);
+      float cosPhi = std::cos(phi);
+      return vec3f(cosPhi * sinTheta, sinPhi * sinTheta, cosTheta);
+    }
+
+    static vec3f uniformSampleSphere(const float radius, const vec2f &s)
+    {
+      const float phi      = float(two_pi) * s.x;
+      const float cosTheta = radius * (1.f - 2.f * s.y);
+      const float sinTheta = 2.f * radius * std::sqrt(s.y * (1.f - s.y));
+      return cartesian(phi, sinTheta, cosTheta);
+    }
+
+// -------------------------------------------------------------------------
+// And the Renderer
 class PathTraceRenderer : public RayMarchVolRenderer {
     public:
         PathTraceRenderer(Domain dom);
+        // these variables are stored in a parameters object in the original
+        // ospray examples code. I want them here in the path tracer. 
+        // they need to be initialized in the constructor. changing
+        // the values will yield different rendering behavior. 
         float sigmaTScale;
+        float sigmaSScale;
+        float ambientLightIntensity;
+        int maxNumScatters;
         //~PathTraceRenderer();
+        // methods
         void RenderFrame();
         void RenderPixel(size_t seed, Ray &r, vec4f &rgba, float &weight);
-        vec3f integrate(RNG &rng, Ray &r , int scatterIndex, int &maxScatterIndex);
+        vec3f integrate(RNG &rng, Ray &r , int scatterIndex, int &maxScatterIndex) ;
         bool sampleWoodcock(RNG &rng, Ray &r, range1f &hits, float &t, float &sample, float &transmittance );
 };
